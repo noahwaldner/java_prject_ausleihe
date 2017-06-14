f5741e2a22326e91ab495f89f90ea7edb58c2262
etienneroulet <eti>
2017-06-14 13:59:59 +0200

    hakjdfjkljkljklj

--- src/gui/Views/addproduct.java
+++ src/gui/Views/addproduct.java
@@ -1,11 +1,15 @@
 package gui.Views;
 
 import javax.swing.*;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 /**
  * Created by Noah Waldner on 19.05.2017.
  */
 public class addproduct extends JPanel {
+    private JTextField textField;
+    private JTextField textField_1;
 
 
 
@@ -14,23 +18,72 @@ public class addproduct extends JPanel {
 
         this.setBounds(244, 0, 656, 600);
 
+        textField = new JTextField();
+        textField.setToolTipText("Titel des Produktes");
+        textField.setColumns(10);
 
-        JLabel lblNewLabel = new JLabel("addprodukt");
-        lblNewLabel.setIcon(new ImageIcon("C:\\Users\\super\\Desktop\\2.jpg"));
-        this.add(lblNewLabel);
+        JLabel lblNewLabel = new JLabel("Titel:");
+
+        JLabel lblNewLabel_1 = new JLabel("Herrsteller:");
+
+        textField_1 = new JTextField();
+        textField_1.setColumns(10);
+
+        JTextArea textArea = new JTextArea();
+        textArea.setBorder(UIManager.getBorder("TextField.border"));
+
+        JLabel lblBeschreibung = new JLabel("Beschreibung:");
+
+        JLabel lblNewLabel_2 = new JLabel("New label");
+
+        JButton button = new JButton("add");
         GroupLayout gl_panel_home = new GroupLayout(this);
         gl_panel_home.setHorizontalGroup(
-                gl_panel_home.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 656, GroupLayout.PREFERRED_SIZE)
+                gl_panel_home.createParallelGroup(Alignment.LEADING)
+                        .addComponent(lblNewLabel_2, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 656, Short.MAX_VALUE)
+                        .addGroup(gl_panel_home.createSequentialGroup()
+                                .addGap(37)
+                                .addGroup(gl_panel_home.createParallelGroup(Alignment.LEADING)
+                                        .addComponent(lblNewLabel)
+                                        .addComponent(lblBeschreibung)
+                                        .addComponent(lblNewLabel_1))
+                                .addPreferredGap(ComponentPlacement.RELATED)
+                                .addGroup(gl_panel_home.createParallelGroup(Alignment.LEADING, false)
+                                        .addComponent(textArea, GroupLayout.DEFAULT_SIZE, 303, Short.MAX_VALUE)
+                                        .addComponent(textField)
+                                        .addComponent(textField_1))
+                                .addContainerGap(222, Short.MAX_VALUE))
+                        .addGroup(Alignment.TRAILING, gl_panel_home.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(button, GroupLayout.PREFERRED_SIZE, 202, GroupLayout.PREFERRED_SIZE))
         );
         gl_panel_home.setVerticalGroup(
-                gl_panel_home.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addComponent(lblNewLabel)
+                gl_panel_home.createParallelGroup(Alignment.LEADING)
+                        .addGroup(Alignment.TRAILING, gl_panel_home.createSequentialGroup()
+                                .addComponent(lblNewLabel_2, GroupLayout.PREFERRED_SIZE, 129, GroupLayout.PREFERRED_SIZE)
+                                .addGap(41)
+                                .addGroup(gl_panel_home.createParallelGroup(Alignment.BASELINE)
+                                        .addComponent(lblNewLabel)
+                                        .addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGroup(gl_panel_home.createParallelGroup(Alignment.BASELINE)
+                                        .addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(lblNewLabel_1))
+                                .addGap(55)
+                                .addGroup(gl_panel_home.createParallelGroup(Alignment.BASELINE)
+                                        .addComponent(lblBeschreibung)
+                                        .addComponent(textArea, GroupLayout.PREFERRED_SIZE, 95, GroupLayout.PREFERRED_SIZE))
+                                .addGap(187)
+                                .addComponent(button)
+                                .addGap(47))
         );
         this.setLayout(gl_panel_home);
 
 
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
 
+            }
 
 
     }
