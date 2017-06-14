68b4a51a3199ad3d06bdfd2fd8caa61d8c98dbaa
noah <noah.waldner>
2017-06-14 16:11:20 +0200

    commit

--- src/gui/Views/Ausleihe.java
+++ src/gui/Views/Ausleihe.java
@@ -11,6 +11,7 @@ import static com.sun.xml.internal.fastinfoset.alphabet.BuiltInRestrictedAlphabe
 
 /**
  * Created by Noah Waldner on 19.05.2017.
+ * Die Klasse Stellt eine Liste mit allen Produkten dar. Es gibt einen Button mit welchen das gewählte Produkt Ausgeleiht oder zurückgegeben werden kann.
  */
 public class Ausleihe extends JPanel implements ActionListener{
 
@@ -18,14 +19,15 @@ public class Ausleihe extends JPanel implements ActionListener{
     JTable table;
 
 
-
+    //Im Konstruktor wird die View zusammengebaut.
     public Ausleihe() {
 
-
+        //Panel Settings
         this.setBounds(244, 0, 656, 600);
         this.setLayout(new BorderLayout());
 
 
+        //JTable von einem Model in einem ScrolPane generieren und zum Panel hinzufügen.
 
         table = new JTable();
         table.setBounds(30,40,200,300);
@@ -35,7 +37,7 @@ public class Ausleihe extends JPanel implements ActionListener{
 
 
 
-
+        //Button zum ausleihen und zurückgeben generieren und zum Panel hinzufügen.
         btn_action = new JButton("Ausleihen / Rückgeben");
         this.add(btn_action, BorderLayout.SOUTH);
         btn_action.addActionListener(this);
@@ -44,10 +46,12 @@ public class Ausleihe extends JPanel implements ActionListener{
 
     }
 
-
+        //Ein actionlistener warted auf eine Interkation mit dem Button.
         public void actionPerformed(ActionEvent e){
             if (e.getSource() == btn_action){
+                //Es wird geprüft ob etwas ausgewählt ist.
                 if (table.getSelectedRow() != -1){
+                    //Es wird das Model des JTable geholt und die Methode AusleiheByIndex darin aufgerufen. Es wird der Index der ausgewählten Reihe mitgegeben.
                     ((TableModel)table.getModel()).ausleiheByIndex(table.getSelectedRow());
 
                 }
